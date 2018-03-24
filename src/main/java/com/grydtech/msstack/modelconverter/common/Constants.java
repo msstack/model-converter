2205a97f399a1ea7af1eb1274bc8e71c3bd5d0f0
randilfernando <randil.fernando@outlook.com>
2018-03-24 11:52:00 +0530

    generate attribute type name with _entity when the type is a entity

--- src/main/java/com/grydtech/msstack/modelconverter/common/Constants.java
+++ src/main/java/com/grydtech/msstack/modelconverter/common/Constants.java
@@ -1,10 +1,23 @@
 package com.grydtech.msstack.modelconverter.common;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Constants {
     public final static String SERVICE_SUFFIX = "_service";
     public final static String HANDLER_CLASS_SUFFIX = "_handler";
     public final static String ENTITY_CLASS_SUFFIX = "_entity";
     public final static String EVENT_CLASS_SUFFIX = "_event";
+    public final static List<String> BASE_TYPES = new ArrayList<>();
+
+    static {
+        BASE_TYPES.add("string");
+        BASE_TYPES.add("integer");
+        BASE_TYPES.add("double");
+        BASE_TYPES.add("long");
+        BASE_TYPES.add("float");
+        BASE_TYPES.add("boolean");
+    }
 
     private Constants(){}
 }
